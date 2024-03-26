db0b7bd20eb4cc90b59dd703e15e152bd17360c5
Youssef Elakhdari <youssefakhdari19@gmail.com>
2024-03-26 14:53:36 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+  <div class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div class="max-w-lg w-full overflow-hidden">
+    <div class="px-6 py-8">
+      <h2 class="text-3xl font-bold mb-4">Welcome to Your Notes App</h2>
+      <p class="text-gray-700 mb-6">Start organizing your thoughts, ideas, and tasks with our simple notes app.</p>
     </div>
+    </div>
+</div>
   )
 }
 
